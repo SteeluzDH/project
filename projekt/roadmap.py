f7395f9133eec7a554933090f1a4fa0469e63644
David <86667747+SteeluzDH@users.noreply.github.com>
2024-10-15 10:19:34 +0200

    Add files via upload

---
+++ projekt/roadmap.py
@@ -0,0 +1,33 @@
+'''
+Agenda idag:
+    - Hur långt har Muhannad kommit? -fortsätter arbeta på pipeline, pratar med Parsan om integrering.
+    - Hur långt har Parsan kommit? - 
+    - Hur långt har David kommit? -
+    - Hitta en uppgift eller ett område Ming kan hjälpa till med.
+        
+
+App
+    start commando: röst / Knapp    -Klar
+        - spelar in wav fil 5 sekunder     -Klar
+            - tar in tonhöjd
+                -bestämmer känsla
+                    -om jag är glad -> happy-mix / upbeat / romantic
+                        -väljer spellista
+                            -starta spellista.
+    start/stop commando
+
+möjlighet att "lyssna" hela tiden (Osäker just nu)
+
+
+byter backgrund/färg med känsla.    -Klar med färgskifte.
+
+
+skriver ut namnet på spellista.     -jobbar med playlist lable
+    
+
+stop commando: Knapp.   -stoppar inspelning automatiskt efter 6 sekunder.
+
+
+
+
+'''
